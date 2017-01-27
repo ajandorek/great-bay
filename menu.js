ca8b37feea515abe1137f0f0a82acc967ef41944
Alec Jandorek <ajandorek@gmail.com>
2017-01-26 20:55:30 -0600

    bid function

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var colors = require("colors");
+var mysql = require("mysql")
 
 var bidOrPost = function() {
     inquirer.prompt([
@@ -11,6 +12,30 @@ var bidOrPost = function() {
         choices: ["Bid".bgRed, "Post".bgBlue]
     }
     ]).then(function(response){
+        if (response.choices === 'Bid'){
+            var connection = mysql.createConnection({
+                host: "localhost",
+                port: 3306,
+
+                user: "root",
+
+                password: "MyNewPass",
+                database: "icecream_db"
+            });
+
+            connection.connect(function(err){
+                if (err) throw err;
+                console.log("connected as id " + connection.threadId);
+            });
+            var currentItems = function() {
+                
+                inquirer.prompt([
+                    {
+
+                    }
+                ])
+            }
+        }
 
     });
 }
