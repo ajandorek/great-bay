efbccded8b047360132cff1b2b426e16b6cca4a6
Alec Jandorek <ajandorek@gmail.com>
2017-01-26 20:50:45 -0600

    Colors

--- menu.js
+++ menu.js
@@ -1,4 +1,5 @@
 var inquirer = require("inquirer");
+var colors = require("colors");
 
 var bidOrPost = function() {
     inquirer.prompt([
@@ -7,7 +8,7 @@ var bidOrPost = function() {
         type: "list",
         name: "bid/post",
         message: "Do you want to bid or post an item?",
-        choices: ["Bid", "Post"]
+        choices: ["Bid".bgRed, "Post".bgBlue]
     }
     ]).then(function(response){
 
